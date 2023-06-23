c3d92a8f2784ca452a45421bb37bc623d5b41619
junyano3 <sonecchi1117@gmail.com>
2023-06-23 21:44:50 +0900

    sellingpricesのnewアクション

--- app/controllers/selling_prices_controller.rb
+++ app/controllers/selling_prices_controller.rb
@@ -2,4 +2,18 @@ class SellingPricesController < ApplicationController
   def index
     @selling_prices= SellingPrice.all
   end
+
+  def new
+    @selling_price = SellingPrice.new
+  end
+
+  def create
+    @selling_price = SellingPrice.new(selling_price_params)
+    if @selling_price.save
+      redirect_to selling_prices_path
+    else
+      render :new
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: "selling_prices#index"
-  resources :selling_price, only: :index
+  resources :selling_prices, only: [:index, :new, :create]
 end
