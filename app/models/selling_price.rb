5b719011e84416301ec99e7662c27e363b7d78a9
junyano3 <sonecchi1117@gmail.com>
2023-06-24 11:46:35 +0900

    モデルのバリデーション
    バリデーションとエラーメッセージの表記
    現時点ではログインユーザーではないためbelongs_to :userもコメントアウト

--- app/models/selling_price.rb
+++ app/models/selling_price.rb
@@ -1,3 +1,7 @@
 class SellingPrice < ApplicationRecord
-  
+  validates :product_name, presence: true
+  validates :high_portion, :good_portion, :normal_portion, :bad_portion, :minced_portion, presence: true, format: { with: /\A\d+\z/, message: "は半角数字のみ入力してください" }
+ 
+  # belongs_to :user
+  # has_many :cuts
 end
