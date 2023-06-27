3452dacbef82a2ec54c9f508b4ae2160838fc3db
junyano3 <sonecchi1117@gmail.com>
2023-06-27 17:37:38 +0900

    歩留り入力フォームの雛形作成

--- app/controllers/cuts_controller.rb
+++ app/controllers/cuts_controller.rb
@@ -1,2 +1,15 @@
 class CutsController < ApplicationController
+  def new
+    @cut = Cut.new
+  end
+
+  def create
+    # @selling_price = SellingPrice.new(selling_price_params)
+    # if @selling_price.save
+    #   redirect_to selling_prices_path
+    # else
+    #   render :new
+    # end
+  end
+
 end
